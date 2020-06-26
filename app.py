aeb985463f341a04c857ac8522ffe725d3933792
Sebastian Torres <sebastian_torres@hotmail.se>
2020-06-26 20:27:08 +0000

    MongoDB app created

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'book_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:<password>@myfirstcluster-x0xst.mongodb.net/book_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def hello():
-    return "Hello world"
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
