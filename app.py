fa9968d980a0e9c85ded72f1eddd9ddefe0c57c7
Sebastian Torres <sebastian_torres@hotmail.se>
2020-06-30 12:58:26 +0000

    routes added for .html files

--- app.py
+++ app.py
@@ -15,6 +15,38 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
+@app.route('/book_gallery')
+def get_tasks():
+    return render_template("bookgallery.html")
+
+@app.route('/get_users')
+def get_users():
+    return render_template("tasks.html", users=mongo.db.users.find())
+
+@app.route('/add_book')
+def get_tasks():
+    return render_template("addbook.html")
+
+@app.route('/login')
+def get_tasks():
+    return render_template("login.html")
+
+@app.route('/signup')
+def get_tasks():
+    return render_template("signup.html")
+
+@app.route('/landing_page')
+def get_tasks():
+    return render_template("index.html")
+
+@app.route('/personal_account')
+def get_tasks():
+    return render_template("personalaccount.html")
+
+@app.route('/reviews')
+def get_tasks():
+    return render_template("reviews.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=(os.environ.get('PORT')),
