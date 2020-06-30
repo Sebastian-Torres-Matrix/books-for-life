6c409cd29f19fdde1a82e75796c5505d7c46b0d6
Sebastian Torres <sebastian_torres@hotmail.se>
2020-06-30 14:34:25 +0000

    Search bar added in bookgallery

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+
+
+// Search Bar
+  $(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Mindset": null,
+        "Grit": null,
+        "Awaken The Giant Within": null
+      },
+    });
+  });
