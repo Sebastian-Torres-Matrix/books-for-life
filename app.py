ceedece1658114b66fc5339cb329daf86f1242de
Sebastian Torres <sebastian_torres@hotmail.se>
2020-07-05 22:13:46 +0000

    logout route added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, flash, redirect, request, url_for
+from flask import Flask, render_template, flash, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from forms import LoginForm, RegistrationForm
@@ -7,17 +7,23 @@ from forms import LoginForm, RegistrationForm
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'book_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:<password>@myfirstcluster-x0xst.mongodb.net/book_manager?retryWrites=true&w=majority'
+#app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-x0xst.mongodb.net/book_manager?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+app.secret_key = os.getenv('SECRET_KEY')
 
 mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/landing_page')
+def landing_page():
+    return render_template("index.html")
+
 @app.route('/get_reviews')
-def get_tasks():
+def get_reviews():
     return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
 @app.route('/book_gallery')
-def get_tasks():
+def book_gallery():
     return render_template("bookgallery.html")
 
 @app.route('/get_users')
@@ -25,27 +31,28 @@ def get_users():
     return render_template("tasks.html", users=mongo.db.users.find())
 
 @app.route('/add_book')
-def get_tasks():
+def add_book():
     return render_template("addbook.html")
 
 @app.route('/login')
-def get_tasks():
+def login():
     return render_template("login.html")
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('landing_page'))
+
 @app.route('/signup')
-def get_tasks():
+def signup():
     return render_template("signup.html")
 
-@app.route('/landing_page')
-def get_tasks():
-    return render_template("index.html")
-
 @app.route('/personal_account')
-def get_tasks():
+def personal_account():
     return render_template("personalaccount.html")
 
 @app.route('/reviews')
-def get_tasks():
+def reviews():
     return render_template("reviews.html")
 
 if __name__ == '__main__':
